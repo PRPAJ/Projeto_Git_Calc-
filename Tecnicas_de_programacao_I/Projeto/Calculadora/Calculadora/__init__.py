c83bd845b067b66c1d192c5c5a30fb71eccccd07
PAULO JR <paulo.pj.alves@gmail.com>
2022-10-30 11:47:38 -0300

    Update __init__.py
    Adicionando funções na pasta init

--- Tecnicas_de_programacao_I/Projeto/Calculadora/Calculadora/__init__.py
+++ Tecnicas_de_programacao_I/Projeto/Calculadora/Calculadora/__init__.py
@@ -1,7 +1,11 @@
-from .funcoes import soma
-
 def calcule(a, b, op):
     if op == '+' or 'soma':
         return soma(a,b)
-    
-    
+    elif op == '-' or 'subtracao':
+        return subtracao(a,b)
+    elif op == '*' or 'multiplicacao':
+        return multiplicacao(a,b)
+    elif op == '/' or 'divisao':
+        return divisao(a,b)
+    else:
+        print('Operação inválida')
